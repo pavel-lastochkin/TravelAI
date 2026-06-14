4c67fb07c147199d542aa385fef107a873ce6714
LaSStiK <LaSStiK@ya.ru>
2026-06-14 13:08:44 +0300

    Initial Commit

---
+++ Travel AI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Travel AI
+//
+//  Created by Pavel Lastochkin on 14.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Travel AI/Travel_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_AIApp.swift
+//  Travel AI
+//
+//  Created by Pavel Lastochkin on 14.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct Travel_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
