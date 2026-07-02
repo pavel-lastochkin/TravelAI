63fcda32ac883f8b74fd3bb765748c5e5b776f52
LaSStiK <LaSStiK@ya.ru>
2026-07-02 19:14:28 +0300

    Icon))

--- Travel AI/Travel_AIApp.swift
+++ Travel AI/Travel_AIApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct Travel_AIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                ExplorePlaceView()
+            }
         }
     }
 }
